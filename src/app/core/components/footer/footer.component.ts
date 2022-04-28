afdf41837312172d1aad64ca60805b8b1e671649
cheerfulperson <yagor.baradavkin@gmail.com>
2022-04-29 00:05:48 +0300

    feat: add footer styles

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FooterComponent {
+  public githubs: Array<{ [key: string]: string }> = [
+    { link: 'https://github.com/Serhii1108', name: 'Sergey' },
+    { link: 'https://github.com/VVK1978', name: 'Victor' },
+    { link: 'https://github.com/cheerfulperson', name: 'Egor' },
+  ];
 }
