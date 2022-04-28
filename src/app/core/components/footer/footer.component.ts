726be11e76979889e873ee93d5f320236b2eaee1
cheerfulperson <yagor.baradavkin@gmail.com>
2022-04-29 00:34:05 +0300

    feat: add rss logo

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   public githubs: Array<{ [key: string]: string }> = [
-    { link: 'https://github.com/Serhii1108', name: 'Sergey' },
-    { link: 'https://github.com/VVK1978', name: 'Victor' },
-    { link: 'https://github.com/cheerfulperson', name: 'Egor' },
+    { link: 'https://github.com/Serhii1108', name: 'footer.names.s' },
+    { link: 'https://github.com/VVK1978', name: 'footer.names.v' },
+    { link: 'https://github.com/cheerfulperson', name: 'footer.names.e' },
   ];
 }
