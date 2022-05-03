cadc116a90feebafa50c6406a4bb25b7aabfb583
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-05-03 21:07:21 +0300

    refactor: change backend url

--- src/app/auth/interceptors/user.interceptor.ts
+++ src/app/auth/interceptors/user.interceptor.ts
@@ -7,6 +7,7 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class UserInterceptor implements HttpInterceptor {
@@ -14,12 +15,25 @@ export class UserInterceptor implements HttpInterceptor {
     req: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const newReq: HttpRequest<unknown> = req.clone({
+    req = req.clone({
       headers: new HttpHeaders({
         accept: 'application/json',
         'Content-Type': 'application/json',
       }),
     });
-    return next.handle(newReq);
+
+    if (req.url.endsWith('/signin')) {
+      req = req.clone({
+        url: `${environment.basicUrl}/signin`,
+      });
+    }
+
+    if (req.url.endsWith('/signup')) {
+      req = req.clone({
+        url: `${environment.basicUrl}/signup`,
+      });
+    }
+
+    return next.handle(req);
   }
 }
