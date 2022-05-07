e4599eda1143c46b6803d497bf98a5a4c80cb4c3
cheerfulperson <yagor.baradavkin@gmail.com>
2022-05-07 16:20:59 +0300

    fead: add auth guard

--- src/app/project-management-app/project-management-app-routing.module.ts
+++ src/app/project-management-app/project-management-app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { BoardComponent } from './components/board/board.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'boards/:id', component: BoardComponent },
+  { path: 'boards/:id', component: BoardComponent, canActivate: [AuthGuard] },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
